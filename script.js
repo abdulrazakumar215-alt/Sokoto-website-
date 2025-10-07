faf2550d01d0be73ab26cc3706af4e5884a5ba78
abdulrazakumar215-alt <abdulrazakumar215@gmail.com>
2025-10-07 03:54:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 // Za a iya ƙara ayyuka kamar popup ko form validation nan
 console.log("Hisbah Sokoto website loaded.");
+function changeLanguage() {
+  const lang = document.getElementById("language").value;
+  alert("Za a canja harshe zuwa: " + lang);
+  // A nan za a iya ƙara lambar da ke canja abun ciki ko tura zuwa shafi daban
+}
